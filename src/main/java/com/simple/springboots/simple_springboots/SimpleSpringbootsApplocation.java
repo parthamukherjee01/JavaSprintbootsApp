e39135000c11c0cd3ced7856c55e63d5c30a3f20
parthamukherjee01 <33608692+parthamukherjee01@users.noreply.github.com>
2023-03-08 16:12:21 +0530

    Initial commit

---
+++ src/main/java/com/simple/springboots/simple_springboots/SimpleSpringbootsApplocation.java
@@ -0,0 +1,12 @@
+package com.simple.springboots.simple_springboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootsApplocation 
+{
+    public static void main( String[] args ) {
+    	SpringApplication.run(SimpleSpringbootsApplocation.class, args);
+    }
+}
